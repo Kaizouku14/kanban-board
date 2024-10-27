4f9be1037100988f3d63ab43215b996c4ebf74ba
Kaizouku14 <mandaalv72@gmail.com>
2024-10-27 17:50:24 +0800

    rename dashboard route into home

--- src/components/Board/kanban-board.tsx
+++ src/components/Board/kanban-board.tsx
@@ -4,27 +4,30 @@ import React, { useState } from "react";
 import Column from "./task-column";
 
 const DEFAULT_CARDS = [
-  // BACKLOG
-  { title: "Look into render bug in dashboard", id: "1", column: "backlog" },
-  { title: "SOX compliance checklist", id: "2", column: "backlog" },
-  { title: "[SPIKE] Migrate to Azure", id: "3", column: "backlog" },
-  { title: "Document Notifications service", id: "4", column: "backlog" },
   // TODO
+  { title: "Look into render bug in dashboard", id: "1", column: "todo" },
+  { title: "SOX compliance checklist", id: "2", column: "todo" },
+  { title: "[SPIKE] Migrate to Azure", id: "3", column: "todo" },
+
+
+   // DOING
+   {
+    title: "Refactor context providers to use Zustand",
+    id: "8",
+    column: "in-progress",
+  },
+  { title: "Add logging to daily CRON", id: "9", column: "in-progress" },
+
+  // VALIDATE
   {
     title: "Research DB options for new microservice",
     id: "5",
-    column: "todo",
+    column: "validate",
   },
-  { title: "Postmortem for outage", id: "6", column: "todo" },
-  { title: "Sync with product on Q3 roadmap", id: "7", column: "todo" },
+  { title: "Sync with product on Q3 roadmap", id: "7", column: "validate" }, 
+  { title: "Document Notifications service", id: "4", column: "validate" },
+  { title: "Postmortem for outage", id: "6", column: "validate" },
 
-  // DOING
-  {
-    title: "Refactor context providers to use Zustand",
-    id: "8",
-    column: "doing",
-  },
-  { title: "Add logging to daily CRON", id: "9", column: "doing" },
   // DONE
   {
     title: "Set up DD dashboards for Lambda listener",
@@ -67,7 +70,7 @@ const KanbanBoard = () => {
       />
       <Column
         title="Complete"
-        column="completed"
+        column="done"
         headingColor="dark:text-emerald-200 text-emerald-400"
         cards={cards}
         setCards={setCards}
