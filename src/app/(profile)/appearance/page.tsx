1a3988d235a22b268117648324a7e36b9f5803a8
Kaizouku14 <mandaalv72@gmail.com>
2024-11-03 18:57:37 +0800

    removed metadat in appearance page

--- src/app/(profile)/appearance/page.tsx
+++ src/app/(profile)/appearance/page.tsx
@@ -6,11 +6,7 @@ import Dark from "../_components/theme/dark";
 import { Separator } from "@/components/ui/separator";
 import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Theme"
-};
 
 const Page = () => {
   const theme = useTheme();
